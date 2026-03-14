900448770ef5f7ec2e5a1ed780902b34ce44a66a
Ethan2611 <ethanibhanesebhor@gmail.com>
2026-03-14 12:55:34 +0000

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,17 @@
+
+
+
+class Dog():
+    tail=1
+    ears=2
+    legs=4
+    skin="fluffy"
+    def bark(self):
+        print("woof,woof")
+    def run(self):
+        print("Dog runs very fast")
+golden_retrive=Dog()
+husky=Dog()
+print(husky.skin)
+print(golden_retrive.ears)
+husky.bark()
---
+++ object_oriented_programming.py
@@ -0,0 +1,6 @@
+
+
+print(type("Ethan"))
+print(type(1))
+Football=["Ronaldo","Messi","Neymar","Yamal"]
+print(type(Football))
