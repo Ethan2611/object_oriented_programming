dc56d4c9f8f5a44aec30706ce8a29662293365b5
Ethan2611 <ethanibhanesebhor@gmail.com>
2026-04-18 12:51:47 +0100

    Add files via upload

--- L5birthday_animation.py
+++ L5birthday_animation.py
@@ -2,6 +2,7 @@
 
 
 import pygame
+import time
 pygame.init()
 screen=pygame.display.set_mode((500,500))
 pygame.display.set_caption("birthday greeting card")
@@ -23,3 +24,26 @@ while run:
     screen.blit(image1,(0,0))
     screen.blit(text,(45,75))
     pygame.display.update()
+    time.sleep(2)
+
+    image2=pygame.image.load("BirthdayL5/card.jpg")
+    image2=pygame.transform.scale(image2,(width,height))
+    font=pygame.font.SysFont("Arial",30)
+    text=font.render("Happy Birthday",True,"black")
+    text2=font.render("Wishing you a great day!",True,"red")
+    screen.blit(image2,(0,0))
+    screen.blit(text,(175,100))
+    screen.blit(text2,(125,250))
+    pygame.display.update()
+    time.sleep(2)
+
+    image3=pygame.image.load("BirthdayL5/cake.jpg")
+    image3=pygame.transform.scale(image3,(width,height))
+    font=pygame.font.SysFont("Arial",30)
+    text=font.render("Happy Birthday",True,"black")
+    text3=font.render("Hip Hip Hooray",True,"red")
+    screen.blit(image3,(0,0))
+    screen.blit(text3,(175,75))
+    pygame.display.update()
+    time.sleep(2)
+
