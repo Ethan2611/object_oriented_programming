b7b92a2f8a49e7ac8d57d7dc87376928bc0c7a4e
Ethan2611 <ethanibhanesebhor@gmail.com>
2026-05-23 12:55:46 +0100

    Add files via upload

---
+++ colour_squares.py
@@ -0,0 +1,43 @@
+
+
+
+import pygame
+pygame.init()
+from pygame.locals import *
+screen=pygame.display.set_mode((500,500))
+screen.fill("black")
+squarex=80
+squarey=90
+sqaure_wid=80
+square_hi=80
+pygame.display.set_caption("colour changing square")
+current_colour="white"
+
+run=True
+while run:
+    for i in pygame.event.get():
+        if i.type==pygame.QUIT:
+            run=False
+
+        if i.type==pygame.KEYDOWN:
+            if i.key==K_UP:
+                squarey=squarey-20
+        
+            if i.key==K_DOWN:
+                squarey=squarey+20
+            
+            if i.key==K_LEFT:
+                squarex=squarex-20
+            
+            if i.key==K_RIGHT:
+                squarex=squarex+20
+
+
+    screen.fill("black")
+    pygame.draw.rect(screen,current_colour,(squarex,squarey,sqaure_wid,square_hi))
+    
+    pygame.display.update()
+
+        
+
+
