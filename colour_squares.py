b57bd4b52b064a5fe2f705eddd7fac80da91f825
Ethan2611 <ethanibhanesebhor@gmail.com>
2026-05-30 12:13:47 +0100

    Add files via upload

--- colour_squares.py
+++ colour_squares.py
@@ -12,30 +12,51 @@ sqaure_wid=80
 square_hi=80
 pygame.display.set_caption("colour changing square")
 current_colour="white"
-
 run=True
 while run:
     for i in pygame.event.get():
-        if i.type==pygame.QUIT:
-            run=False
+        if i.type == pygame.QUIT:
+            run = False
 
-        if i.type==pygame.KEYDOWN:
-            if i.key==K_UP:
-                squarey=squarey-20
-        
-            if i.key==K_DOWN:
-                squarey=squarey+20
+        if i.type == pygame.KEYDOWN:
+            if i.key == K_UP:
+                squarey = squarey - 20
+
+            if i.key == K_DOWN:
+                squarey = squarey + 20
+
+            if i.key == K_LEFT:
+                squarex = squarex - 20
+
+            if i.key == K_RIGHT:
+                squarex = squarex + 20
+    
+    if squarex <= 0:
+        current_colour = "red"
+
+    elif squarex + sqaure_wid >= 500:
+        current_colour = "blue"
+
+    elif squarey <= 0:
+        current_colour = "yellow"
+
+    elif squarey + square_hi >= 500:
+        current_colour = "green"
+
+    else:
+        current_colour = "white"
+
+    screen.fill("black")
+    pygame.draw.rect(screen, current_colour,
+                     (squarex, squarey, sqaure_wid, square_hi))
+
+    pygame.display.update()
             
-            if i.key==K_LEFT:
-                squarex=squarex-20
             
-            if i.key==K_RIGHT:
-                squarex=squarex+20
 
 
     screen.fill("black")
     pygame.draw.rect(screen,current_colour,(squarex,squarey,sqaure_wid,square_hi))
-    
     pygame.display.update()
 
         
